4b4cb80bb0d421f513d2e144c08818437dc0b237
kyle589 <kyleng589@gmail.com>
2022-02-17 01:32:50 -0800

    makefile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import static org.junit.Assert.*;
 import org.junit.*;
+
 //javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+// /software/CSE/oracle-java-se-14/jdk-14.0.2/bin/javac
 //scp -r . cs15lwi22akq@ieng6.ucsd.edu:~/markdown-parse; ssh cs15lwi22akq@ieng6.ucsd.edu "cd markdown-parse; javac MarkdownParse.java; java MarkdownParse test-file.md"
-//ssh cs15lwi22akq@ieng6.ucsd.edu "cd markdown-parse && javac MarkdownParse.java"
+//scp -r . cs15lwi22akq@ieng6.ucsd.edu:~/markdown-parse; ssh cs15lwi22akq@ieng6.ucsd.edu "cd markdown-parse; /software/CSE/oracle-java-se-14/jdk-14.0.2/bin/javac MarkdownParse.java; /software/CSE/oracle-java-se-14/jdk-14.0.2/bin/javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java; /software/CSE/oracle-java-se-14/jdk-14.0.2/bin/java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest"
 public class MarkdownParseTest {
     @Test
     public void addition() {
